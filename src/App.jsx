68a31c53ddb451870d358395e116a1e80c178331
vikasdagur369 <vikasdagur369@gmail.com>
2025-02-26 18:11:55 +0530

    refactor: replace local state management with Zustand for count functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useCount } from "./store/countStore";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, incCount, decCount } = useCount();
 
   return (
     <>
@@ -18,9 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => incCount(3)}>count is {count}</button>
+        <button onClick={() => decCount(3)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/countStore.js
+++ src/store/countStore.js
@@ -2,7 +2,11 @@ import { create } from "zustand";
 
 export const useCount = create((set) => ({
   count: 0,
-  incCount: () => {
-    set((state) => ({ ...state, count: state.count + 1 }));
+  incCount: (num) => {
+    set((state) => ({ ...state, count: state.count + num }));
+  },
+
+  decCount: (num) => {
+    set((state) => ({ ...state, count: state.count - num }));
   },
 }));
